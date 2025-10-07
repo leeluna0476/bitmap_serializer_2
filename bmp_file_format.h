ab1aec7ac734aa6bcaf24cf4219042442be7f389
LEE LUNA <seojilee@student.42seoul.kr>
2025-10-07 15:21:37 +0900

    british style

--- bmp_file_format.h
+++ bmp_file_format.h
@@ -22,14 +22,14 @@ struct	BmpInfoHeader
 	uint32_t	size;					// the size of this header, in bytes (40)
 	int32_t		width;					// the bitmap width in pixels (signed integer)
 	int32_t		height;					// the bitmap height in pixels (signed integer)
-	uint16_t	color_plane;			// the number of color planes (must be 1)
-	uint16_t	bits_per_pixel;			// the number of bits per pixel, which is the color depth of the image.
+	uint16_t	colour_plane;			// the number of colour planes (must be 1)
+	uint16_t	bits_per_pixel;			// the number of bits per pixel, which is the colour depth of the image.
 	uint32_t	compression;			// the compression method being used.
 	uint32_t	image_size;				// the image size. This is the size of the raw bitmap data.
 	int32_t		horizontal_resolution;	// the horizontal resolution of the image. (pixel per metre, signed integer)
 	int32_t		vertical_resolution;	// the vertical resolution of the image. (pixel per metre, signed integer)
-	uint32_t	color_number;			// the number of colors in the color palette, or 0 to default to 2n
-	uint32_t	color_number_important;	// the number of important colors used, or 0 when every color is important; generally ignored
+	uint32_t	colour_number;			// the number of colours in the colour palette, or 0 to default to 2n
+	uint32_t	colour_number_important;	// the number of important colours used, or 0 when every colour is important; generally ignored
 };
 #pragma pack(pop)
 
